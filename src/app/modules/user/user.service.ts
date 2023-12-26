782874d38cfbd32a4551b3ab98cde41fb59177c8
Prottasa Karim <prottasakarim99@gmail.com>
2023-12-27 04:00:21 +0600

    token is done

--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -4,6 +4,7 @@ import { TLoginUser, TUser } from './user.interface';
 import { User } from './user.model';
 import config from '../../config';
 import bcrypt from 'bcrypt'
+import { createToken } from './user.utilies';
 
 const createUserIntoDB = async (payload: Partial<TUser>) => {
   const result = await User.create(payload);
@@ -13,18 +14,30 @@ const createUserIntoDB = async (payload: Partial<TUser>) => {
 const loginUser = async (payload: TLoginUser) => {
   console.log("payload",payload)
   const username = payload.username;
-  const password = payload.username;
-  const user = await User.findOne({ username: username },{username:1,password:1});
+  const password = payload.password;
+  const user = await User.findOne({ username: username },{username:1,password:1,role:1,email:1});
   console.log('user',user)
 
   if (!user) {
     throw new AppError(httpStatus.NOT_FOUND, 'This user is not found !');
   }
-  const hashPassword= await bcrypt.hash( password,Number(config.bcrypt_salt_rounds))
-  if(user.password!==hashPassword){
+ 
+  const match = await bcrypt.compare(password, user.password);
+  if(!match){
     throw new AppError(httpStatus.NOT_FOUND, 'Give Correct Password');
   }
-  
+  const jwtPayload = {
+    _id: user._id,
+    role: user.role,
+    email:user.email
+  };
+
+  const accessToken = createToken(
+    jwtPayload,
+    config.jwt_access_secret as string,
+    config.jwt_access_expires_in as string,
+  );
+  console.log(accessToken)
 
   return null
 };
